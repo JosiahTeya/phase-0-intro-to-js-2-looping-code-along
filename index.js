16d3da864238e7de53c18e9b6546738fbda371af
Josiah Teya <teyajosiah02@gmail.com>
2023-03-28 12:40:17 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,23 +1,4 @@
-// Code your solutions in this file
-// for (let age = 30; age < 40; age ++) {
-//     console.log(`I'm ${age} years old. Happy birthday to me!`);
-    
-// }
-// const gifts = ["teddy bear", "drone", "doll"];
-// function wrapGifts(gifts) {
-//     for (let i=0; i<gifts.length; i++){
-//         console.log(`Wrapped ${gifts[i]} and added a bow!`);
-//     }
-//     return gifts;
-// }
-// wrapGifts(gifts);
-// const cards = ["Guadalupe", "Ollie", "Aki"];
-// function writeCards(cards) {
-//     for (let i=0; i<cards.length; i++){
-//         console.log(`Thank you, ${cards[i]}, for the wonderful surprise gift!`);
-//     }
-//     return cards;
-// }
+
 function writeCards(namesArray, eventName) {
     const messages =[];
     for (let i=0; i<namesArray.length; i++){
